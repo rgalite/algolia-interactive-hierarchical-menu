bb6e09e1b400c51602f1fb127db4cf3f0718fa31
Rudth-Mael <rm@galite.net>
2019-02-14 18:53:24 +0100

    Added show more button + prettified stuff

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const searchClient = algoliasearch(
-  "latency",
-  "6be0576ff61c053d5f9a3225e2a90f76"
-);
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+)
 
 const search = instantsearch({
-  indexName: "instant_search",
-  searchClient
-});
+  indexName: 'instant_search',
+  searchClient,
+})
 
 search.addWidget(
   instantsearch.widgets.hits({
-    container: "#hits",
+    container: '#hits',
     templates: {
       item: hit => `
       <div class="hit">
@@ -25,22 +25,22 @@ search.addWidget(
           <div class="hit-price">$${hit.price}</div>
         </div>
       </div>
-      `
-    }
+      `,
+    },
   })
-);
+)
 
 search.addWidget(
   new customAlgolia.HierarchicalMenuWidget({
-    container: "#breadcrumb",
+    container: '#breadcrumb',
     attributes: [
-      "hierarchicalCategories.lvl0",
-      "hierarchicalCategories.lvl1",
-      "hierarchicalCategories.lvl2",
-      "hierarchicalCategories.lvl3"
+      'hierarchicalCategories.lvl0',
+      'hierarchicalCategories.lvl1',
+      'hierarchicalCategories.lvl2',
+      'hierarchicalCategories.lvl3',
     ],
-    showMoreLimit: 8
+    showMoreLimit: 8,
   })
-);
+)
 
-search.start();
+search.start()
--- mg.hierarchical-menu-widget.js
+++ mg.hierarchical-menu-widget.js
@@ -1,4 +1,4 @@
-window.customAlgolia = {};
+window.customAlgolia = {}
 window.customAlgolia.HierarchicalMenuWidget = function({
   container,
   attributes,
@@ -8,154 +8,178 @@ window.customAlgolia.HierarchicalMenuWidget = function({
     limit: 10,
     showMore: true,
     showMoreLimit: 10,
-    separator: " > ",
+    separator: ' > ',
     rootPath: null,
     //    showParentLevel: false,
     //    sortBy: () => {},
     templates: {},
     cssClasses: {
-      returnButton: "return-button",
-      titleWrapper: "title",
-      rootTitleWrapper: "title title--centered",
-      list: "category-list",
-      item: "category"
-    }
+      returnButton: 'return-button',
+      titleWrapper: 'title',
+      rootTitleWrapper: 'title title--centered',
+      list: 'category-list',
+      item: 'category',
+      showMoreLink: 'show-all-trigger',
+    },
+    showMoreText: 'Show more',
     //    transformItems: () => {}
-  };
+  }
 
   this.options = {
     ...defaultOptions,
     ...optionalParameters,
     container,
-    attributes
-  };
-
-  this.complete = true;
-};
+    attributes,
+  }
+}
 
 window.customAlgolia.HierarchicalMenuWidget.prototype.getConfiguration = function() {
   return {
-    facets: this.options.attributes
-  };
-};
+    facets: this.options.attributes,
+  }
+}
 
 window.customAlgolia.HierarchicalMenuWidget.prototype.attachPreviousClickEvent = function({
   state,
   helper,
   button,
-  currentLevel
+  currentLevel,
 }) {
-  button.addEventListener("click", e => {
+  button.addEventListener('click', e => {
     helper
       .removeFacetRefinement(this.options.attributes[currentLevel - 1])
-      .search();
-  });
-};
+      .search()
+  })
+}
 
 window.customAlgolia.HierarchicalMenuWidget.prototype.init = function({
   helper,
-  state
+  state,
 }) {
   this.container =
-    typeof this.options.container === "string"
+    typeof this.options.container === 'string'
       ? document.querySelector(this.options.container)
-      : this.options.container;
-};
+      : this.options.container
+}
 
 window.customAlgolia.HierarchicalMenuWidget.prototype.getSelectedFacetsName = function({
   level,
-  facetsRefinements
+  facetsRefinements,
 }) {
-  const facetRefinementName = this.options.attributes[level];
+  const facetRefinementName = this.options.attributes[level]
 
   return facetsRefinements[facetRefinementName]
     .map(facetName => {
-      const splittedFacetName = facetName.split(this.options.separator);
-      return splittedFacetName[splittedFacetName.length - 1];
+      const splittedFacetName = facetName.split(this.options.separator)
+      return splittedFacetName[splittedFacetName.length - 1]
     })
-    .join(", ");
-};
+    .join(', ')
+}
+
+window.customAlgolia.HierarchicalMenuWidget.prototype.getShowAllLink = function() {
+  const link = document.createElement('a')
+  const container = this.container
+  const cssClasses = this.options.cssClasses
+
+  link.innerHTML = this.options.showMoreText
+  link.setAttribute('class', this.options.cssClasses.showMoreLink)
+  link.addEventListener('click', function(event) {
+    container.querySelectorAll(`.${cssClasses.item}`).forEach(facetLink => {
+      facetLink.setAttribute(
+        'style',
+        (facetLink.getAttribute('style') || '').replace('display: none;', '')
+      )
+    })
+    link.setAttribute('style', 'display: none;')
+  })
+
+  return link
+}
 
 window.customAlgolia.HierarchicalMenuWidget.prototype.render = function({
   results,
   helper,
-  state
+  state,
 }) {
-  const currentLevel = Object.keys(state.facetsRefinements).length;
-  this.container.innerHTML = "";
+  const currentLevel = Object.keys(state.facetsRefinements).length
+  this.container.innerHTML = ''
 
   // Add a previous button
   if (currentLevel) {
     const facetName = this.getSelectedFacetsName({
       level: currentLevel - 1,
-      facetsRefinements: state.facetsRefinements
-    });
+      facetsRefinements: state.facetsRefinements,
+    })
 
-    const prevButton = document.createElement("a");
-    prevButton.setAttribute("class", this.options.cssClasses.returnButton);
-    prevButton.innerHTML = `&lt;`;
+    const prevButton = document.createElement('a')
+    prevButton.setAttribute('class', this.options.cssClasses.returnButton)
+    prevButton.innerHTML = `&lt;`
 
     this.attachPreviousClickEvent({
       helper,
       state,
       currentLevel,
-      button: prevButton
-    });
+      button: prevButton,
+    })
 
-    const title = document.createElement("span");
-    title.innerHTML = facetName;
+    const title = document.createElement('span')
+    title.innerHTML = facetName
 
-    const rightThingy = document.createElement("span");
-    rightThingy.innerHTML = "&nbsp;";
+    const rightThingy = document.createElement('span')
+    rightThingy.innerHTML = '&nbsp;'
 
-    const titleWrapper = document.createElement("div");
-    titleWrapper.setAttribute("class", this.options.cssClasses.titleWrapper);
+    const titleWrapper = document.createElement('div')
+    titleWrapper.setAttribute('class', this.options.cssClasses.titleWrapper)
 
-    titleWrapper.appendChild(prevButton);
-    titleWrapper.appendChild(title);
-    titleWrapper.appendChild(rightThingy);
+    titleWrapper.appendChild(prevButton)
+    titleWrapper.appendChild(title)
+    titleWrapper.appendChild(rightThingy)
 
-    this.container.appendChild(titleWrapper);
+    this.container.appendChild(titleWrapper)
   } else {
-    const titleWrapper = document.createElement("div");
-    titleWrapper.setAttribute(
-      "class",
-      this.options.cssClasses.rootTitleWrapper
-    );
+    const titleWrapper = document.createElement('div')
+    titleWrapper.setAttribute('class', this.options.cssClasses.rootTitleWrapper)
 
     if (this.options.rootPath) {
-      titleWrapper.innerHTML = this.options.rootPath;
+      titleWrapper.innerHTML = this.options.rootPath
     }
 
-    this.container.appendChild(titleWrapper);
+    this.container.appendChild(titleWrapper)
   }
 
-  const list = document.createElement("div");
-  list.setAttribute("class", this.options.cssClasses.list);
+  const list = document.createElement('div')
+  list.setAttribute('class', this.options.cssClasses.list)
 
-  const facets = results.facets[currentLevel];
+  const facets = results.facets[currentLevel]
 
   if (facets) {
     Object.keys(facets.data).forEach((facetName, index) => {
-      const link = document.createElement("a");
-      const splittedFacetName = facetName.split(this.options.separator);
-      const innerHTML = splittedFacetName[splittedFacetName.length - 1];
+      const link = document.createElement('a')
+      const splittedFacetName = facetName.split(this.options.separator)
+      const innerHTML = splittedFacetName[splittedFacetName.length - 1]
 
-      link.setAttribute("class", this.options.cssClasses.item);
+      link.setAttribute('class', this.options.cssClasses.item)
 
       if (index >= this.options.showMoreLimit) {
-        link.setAttribute("style", "display: none;");
+        link.setAttribute('style', 'display: none;')
       }
 
-      link.innerHTML = `${innerHTML} (${facets.data[facetName]})`;
-      link.addEventListener("click", event => {
-        const facetRefinementName = this.options.attributes[currentLevel];
-        helper.addFacetRefinement(facetRefinementName, facetName).search();
-      });
+      link.innerHTML = `${innerHTML} (${facets.data[facetName]})`
+      link.addEventListener('click', event => {
+        const facetRefinementName = this.options.attributes[currentLevel]
+        helper.addFacetRefinement(facetRefinementName, facetName).search()
+      })
 
-      list.appendChild(link);
-    });
+      list.appendChild(link)
+    })
 
-    this.container.appendChild(list);
+    this.container.appendChild(list)
+
+    if (
+      Object.keys(facets.data).length > this.options.showMoreLimit &&
+      !this.facetsAllDisplayed
+    ) {
+      this.container.appendChild(this.getShowAllLink())
+    }
   }
-};
+}
